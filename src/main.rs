875b56a07d35f970e9e8079b17d5915af9c20798
Sam Liu <ontouchstart@gmail.com>
2026-02-04 15:52:24 -0500

    assert_eq!(h1.get_content().unwrap(), "<h1>Hello, world from Vanilla Rust!</h1>");

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ fn main() {
     let tab = browser.new_tab().expect("Could not create tab");
     let url = "https://ontouchstart.github.io/trunk-hello-world".to_string();
     tab.navigate_to(&url).expect("Could not navigate to url");
+    let h1 = tab.wait_for_element("h1").expect("Could not get h1");
+    assert_eq!(h1.get_content().unwrap(), "<h1>Hello, world from Vanilla Rust!</h1>");
     let png_data = tab
         .wait_for_element("body").expect("Could not get body")
-        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png).expect("Cound not genearte PNG");
+    .capture_screenshot(Page::CaptureScreenshotFormatOption::Png).expect("Cound not genearte PNG");
     std::fs::write("screenshot.png", png_data).expect("Could not save png");
 }
