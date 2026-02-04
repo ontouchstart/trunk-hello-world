72f0122ed1de525e5beda3fa2c26f2a3b31e33d8
Sam Liu <ontouchstart@gmail.com>
2026-02-04 11:22:08 -0500

    use panic! to debug

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use anyhow::Result;
-
 use headless_chrome::protocol::cdp::Page;
 use headless_chrome::{Browser, LaunchOptions};
 
-fn main() -> Result<()> {
+fn main() {
     let browser = Browser::new(
         LaunchOptions::default_builder()
             .build()
             .expect("Could not find chrome-executable"),
-    )?;
-    let tab = browser.new_tab()?;
-    tab.navigate_to("https://ontouchstart.github.io/trunk-hello-world")?;
+    ).expect("Could not set browser");
+    let tab = browser.new_tab().expect("Could not create tab");
+    let url = "https://ontouchstart.github.io/trunk-hello-world".to_string();
+    tab.navigate_to(&url).expect("Could not navigate to url");
     let png_data = tab
-        .wait_for_element("body")?
-        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
-    std::fs::write("screenshot.png", png_data)?;
-    Ok(())
+        .wait_for_element("body").expect("Could not get body")
+        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png).expect("Cound not genearte PNG");
+    std::fs::write("screenshot.png", png_data).expect("Could not save png");
 }
