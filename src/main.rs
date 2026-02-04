8731ec33a6409633626fb2cfce4d03e4a72262eb
Sam Liu <ontouchstart@gmail.com>
2026-02-04 16:20:57 -0500

    .sandbox(false)

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use headless_chrome::{Browser, LaunchOptions};
 fn main() {
     let browser = Browser::new(
         LaunchOptions::default_builder()
+            .sandbox(false)
             .build()
             .expect("Could not find chrome-executable"),
-    ).expect("Could not set browser");
+    )
+    .expect("Could not set browser");
     let tab = browser.new_tab().expect("Could not create tab");
     let url = "https://ontouchstart.github.io/trunk-hello-world".to_string();
     tab.navigate_to(&url).expect("Could not navigate to url");
     let h1 = tab.wait_for_element("h1").expect("Could not get h1");
-    assert_eq!(h1.get_content().unwrap(), "<h1>Hello, world from Vanilla Rust!</h1>");
+    assert_eq!(
+        h1.get_content().unwrap(),
+        "<h1>Hello, world from Vanilla Rust!</h1>"
+    );
     let png_data = tab
-        .wait_for_element("body").expect("Could not get body")
-    .capture_screenshot(Page::CaptureScreenshotFormatOption::Png).expect("Cound not genearte PNG");
+        .wait_for_element("body")
+        .expect("Could not get body")
+        .capture_screenshot(Page::CaptureScreenshotFormatOption::Png)
+        .expect("Cound not genearte PNG");
     std::fs::write("screenshot.png", png_data).expect("Could not save png");
 }
